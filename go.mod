b151f5b0dcd413c1ddc904f32789ef62297cdbef
Edouard CLAUDE <edouard@squirrel.fr>
2024-01-18 12:03:16 +0400

    initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Squirrel-Entreprise/paginator
+
+go 1.14
+
+require gorm.io/gorm v1.25.5
---
+++ paginator.go
@@ -0,0 +1,93 @@
+package paginator
+
+import (
+	"errors"
+	"math"
+
+	"gorm.io/gorm"
+)
+
+// Param parameter
+type Param struct {
+	DB      *gorm.DB
+	Page    int
+	Limit   int
+	OrderBy []string
+	ShowSQL bool
+}
+
+// Paginator return
+type Paginator struct {
+	TotalRecord int64       `json:"total_record"`
+	TotalPage   int         `json:"total_page"`
+	Records     interface{} `json:"records"`
+	Offset      int         `json:"offset"`
+	Limit       int         `json:"limit"`
+	Page        int         `json:"page"`
+	PrevPage    int         `json:"prev_page"`
+	NextPage    int         `json:"next_page"`
+}
+
+// Paging Pagination
+func Paging(p *Param, result interface{}) (*Paginator, error) {
+	db := p.DB
+
+	if p.ShowSQL {
+		db = db.Debug()
+	}
+	if p.Page < 1 {
+		p.Page = 1
+	}
+	if p.Limit == 0 {
+		p.Limit = 10
+	}
+	if len(p.OrderBy) > 0 {
+		for _, o := range p.OrderBy {
+			db = db.Order(o)
+		}
+	}
+
+	done := make(chan bool, 1)
+	var paginator Paginator
+	var count int64
+	var offset int
+
+	go countRecords(db, result, done, &count)
+
+	if p.Page == 1 {
+		offset = 0
+	} else {
+		offset = (p.Page - 1) * p.Limit
+	}
+
+	if err := db.Limit(p.Limit).Offset(offset).Find(result).Error; err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, err
+	}
+	<-done
+
+	paginator.TotalRecord = count
+	paginator.Records = result
+	paginator.Page = p.Page
+
+	paginator.Offset = offset
+	paginator.Limit = p.Limit
+	paginator.TotalPage = int(math.Ceil(float64(count) / float64(p.Limit)))
+
+	if p.Page > 1 {
+		paginator.PrevPage = p.Page - 1
+	} else {
+		paginator.PrevPage = p.Page
+	}
+
+	if p.Page == paginator.TotalPage {
+		paginator.NextPage = p.Page
+	} else {
+		paginator.NextPage = p.Page + 1
+	}
+	return &paginator, nil
+}
+
+func countRecords(db *gorm.DB, anyType interface{}, done chan bool, count *int64) {
+	db.Model(anyType).Count(count)
+	done <- true
+}
